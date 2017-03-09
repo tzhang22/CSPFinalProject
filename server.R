661acc39f7a5ae6e2a74501780a12367a8fbfaee
Timothy McAleer <mcaletim@uw.edu>
2017-03-08 23:24:05 -0800

    Deletes some files, and adds all of the code for my part

--- server.R
+++ server.R
@@ -1,100 +1,172 @@
-<<<<<<< HEAD
-library("httr")
-library("jsonlite")
-=======
-# Load the shiny, ggplot2, and dplyr libraries
->>>>>>> master
-library("shiny")
-library("ggplot2")
-library("dplyr")
+max.total.pov <- max(filtered.poverty$`Total Poverty`)
+max.total.18.pov <- max(filtered.poverty$`Total Poverty (Under 18)`)
 
-<<<<<<< HEAD
+mean.table <- group_by(ultra.join, Region) %>% 
+                summarize("mean.percent" = mean(`Percent Total Poverty`), 
+                          "mean.percent.18" = mean(`Percent Total Poverty (Under 18)`))
+
 server <- function(input, output) {
   
   # reactive table that is used in both the table and ggplot
-  filtered <- reactive({
-    new.table <- filter(joined.one, Region %in% input$checkGroup)
-    
-    return(new.table)
+  filtered.map <- reactive({
+      new.table <- ultra.join
+      if (input$radio.region != "All") {
+      new.table <- filter(ultra.join, Region %in% input$radio.region)
+      }
+      return(new.table)
   })
   
-  # The graph, that is based off the reactive data, and inputted color scheme
+  filtered.graph <- reactive({
+    new.chart <- filter(filtered.poverty, State == input$state1 | State == input$state2)
+    return(new.chart)
+  })
+  
+  filter.pie <- reactive({
+    pie.table <- ultra.join  
+    if (input$radio.for.regions != "All") {
+      pie.table <- filter(ultra.join, Region %in% input$radio.for.regions)
+    }  
+    
+    pie.table <- group_by(pie.table, State) %>% 
+                  summarize("state.percent" = mean(`Percent Total Poverty`),
+                           "state.under.18.percent" = mean(`Percent Total Poverty (Under 18)`))
+      
+    return(pie.table)
+  })
+  
+  # The graph that is based off the reactive region
   output$plot <- renderPlot({
     
+    if (input$radio.type == 1) {
+      p <- ggplot(data = filtered.map()) +
+        geom_polygon(aes(x = long, y = lat, group = group, color = Region, 
+                         fill = `Total Poverty`), color = "black") +
+        scale_fill_continuous(low = "#F2F2F2", high = "#901010", 
+                              breaks = c(500000, 2000000,4000000,6000000),
+                              labels = c("500,000", "Two Million", "Four Million", "Six Million"))
+    } else if (input$radio.type == 2) {
+      p <- ggplot(data = filtered.map()) +
+        geom_polygon(aes(x = long, y = lat, group = group, color = Region, 
+                         fill = `Percent Total Poverty`), color = "black") +
+        scale_fill_continuous(low = "#F2F2F2", high = "#672A70")
+    } else if (input$radio.type == 3) {
+      p <- ggplot(data = filtered.map()) +
+        geom_polygon(aes(x = long, y = lat, group = group, 
+                         fill = `Total Poverty (Under 18)`), color = "black") +
+        scale_fill_continuous(low = "#F2F2F2", high = "#E6E151",
+                              breaks = c(1000000,2000000),
+                              labels = c("One Million", "Two Million"))
+    } else {
+      p <- ggplot(data = filtered.map()) +
+        geom_polygon(aes(x = long, y = lat, group = group, 
+                         fill = `Percent Total Poverty (Under 18)`), color = "black") +
+        scale_fill_continuous(low = "#F2F2F2", high = "#138A6E")
+    }
     
-    p <- ggplot(data = filtered()) +
-      geom_point(mapping = aes(x = as.numeric(ESTAB), y = as.numeric(PAYANN), color = Region), size = 3) +
-      xlab("Number of Establishments") +
-      ylab("Annual Payroll ($1,000)")
-    
-#    if (input$color.choice == "Primary") {
-#      p <- p + scale_color_manual(values = c("#EA2828", "#2158C6", "#C6AC19"))
-#    } else if (input$color.choice == "Cool") {
-#      p <- p + scale_color_manual(values = c("#2B59C3", "#3CB8C8", "#500C84"))
-#    } else {
-#      p <- p + scale_color_manual(values = c("#EAD143", "#A41111", "#D96431"))
-#    }
+    p <- p + coord_fixed(ratio = 1.0) +
+             labs(x = "", y = "") +
+             theme_minimal() +
+             theme(axis.title.x = element_blank(),
+                   axis.text.x = element_blank(),
+                   axis.ticks.x = element_blank(),
+                   axis.title.y = element_blank(),
+                   axis.text.y = element_blank(),
+                   axis.ticks.y = element_blank())
     
     return(p)
   })
   
-  # The data below the graph that shows the coordinates of the point
-  output$info <- renderText({
-    xy_str <- function(e) {
-      if(is.null(e)) return("NULL\n")
-      paste0("x=", round(e$x, 1), " y=", round(e$y, 1), "\n")
-    }
-    
-    
-    paste0("click: ", xy_str(input$plot_click))
-  })
-  
   output$summary <- renderText({
     
-    current.species <- function(number) {
-      if (!is.na(input$checkGroup[number])) {
-        paste0(input$checkGroup[number], sep = ", ")
-      }
-    }
-    
-    
-    message <- paste0("This graph shows the petal-length of Irises in relation to the sepal-length,\n", 
-                      "colored by species. The current species are: ", current.species(1), current.species(2),
-                      current.species(3))
+    message <- paste0("This graph shows total population under the poverty line per state")
     
     return(message)
   })
   
-  output$table.summary <- renderText({
-    current.range <- function() {
-      if (!is.na(input$sepal.choice)) {
-        paste0(round(input$sepal.choice[1], 2), " - ", round(input$sepal.choice[2], 2))
-      }
+  output$plot2 <- renderPlot({
+    if (input$radio == 1) {
+      graph <- ggplot(data = filtered.graph()) +
+        geom_bar(mapping = aes(x = State, y = `Total Poverty`,
+                               fill = `Total Poverty`), stat = "identity")
+    } else if (input$radio == 2) {
+      graph <- ggplot(data = filtered.graph()) +
+        geom_bar(mapping = aes(x = State, y = `Percent Total Poverty`,
+                                   fill = `Percent Total Poverty`), stat = "identity")
+    } else if (input$radio == 3) {
+      graph <- ggplot(data = filtered.graph()) +
+        geom_bar(mapping = aes(x = State, y = `Total Poverty (Under 18)`,
+                                   fill = `Total Poverty (Under 18)`), stat = "identity")
+    } else {
+      graph <- ggplot(data = filtered.graph()) +
+        geom_bar(mapping = aes(x = State, y = `Percent Total Poverty (Under 18)`,
+                                   fill = `Percent Total Poverty (Under 18)`), stat = "identity")
     }
     
-    current.species <- function(number) {
-      if (!is.na(input$checkGroup[number])) {
-        paste0(input$checkGroup[number], sep = ", ")
-      }
+    graph <- graph + scale_fill_continuous(low = "#E68669", high = "#185A90",
+                                           breaks = c(),
+                                           labels = c(""))
+    
+    return(graph)
+  })
+  
+  output$plot3 <- renderPlot({
+    if (input$statistic == 1) {
+      my.plot <- ggplot(data = mean.table) +
+        geom_bar(mapping = aes(x = Region, y = mean.percent, fill = Region),
+                 stat = "identity") +
+        labs(y = "Mean Percentage of Population Below Poverty Line")
+    } else {
+      my.plot <- ggplot(data = mean.table) +
+        geom_bar(mapping = aes(x = Region, y = mean.percent.18, fill = Region),
+                 stat = "identity") +
+        labs(y = paste("Mean", input$statistic)) +
+        labs(y = "Mean Percentage of Population Under 18 Below Poverty Line")
     }
-    
-    table.message <- paste0("This table shows the current range of: ", current.range(),
-                            " for sepal length. It is also showing the species: ", current.species(1),
-                            current.species(2), current.species(3))
-    
+    if (input$radio.for.regions == "All") {
+      my.plot <- my.plot + scale_fill_manual(values = c("#E3C73C", "#2821A3", 
+                                             "#0F7D32", "#D73413", "#932F3E"))
+    } else if (input$radio.for.regions == "Midwest") {
+      my.plot <- my.plot + scale_fill_manual(values = c("#E3C73C", "#858585", 
+                                                        "#858585", "#858585", "#858585"))
+    } else if (input$radio.for.regions == "Northeast") {
+      my.plot <- my.plot + scale_fill_manual(values = c("#858585", "#2821A3", 
+                                                        "#858585", "#858585", "#858585"))
+    } else if (input$radio.for.regions == "Northwest") {
+      my.plot <- my.plot + scale_fill_manual(values = c("#858585", "#858585", 
+                                                        "#0F7D32", "#858585", "#858585"))
+    } else if (input$radio.for.regions == "South") {
+      my.plot <- my.plot + scale_fill_manual(values = c("#858585", "#858585", 
+                                                        "#858585", "#D73413", "#858585"))
+    } else {
+        my.plot <- my.plot + scale_fill_manual(values = c("#858585", "#858585", 
+                                                          "#858585", "#858585", "#932F3E"))
+    }
+    return(my.plot)
   })
   
-  # Generate an table view of the reactive data
-  output$table <- renderTable({
-    data.frame(x=filtered())
+  output$pie.plot <- renderPlotly({
+    if (input$radio.for.regions == "All") {
+      my.pie <- plot_ly(filter.pie(), labels = ~c(""), values = ~c(""), type = 'pie') %>%
+        layout(title = '',
+               xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+               yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+    } else {
+      if (input$statistic == 1) {
+        my.pie <- plot_ly(filter.pie(), labels = ~State, values = ~state.percent, type = 'pie') %>%
+          layout(title = 'Percentage Under Poverty Line For Region',
+                 xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                 yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+      } else {
+        my.pie <- plot_ly(filter.pie(), labels = ~State, values = ~state.under.18.percent, type = 'pie') %>%
+          layout(title = 'Percentage Under Poverty Line (Under 18) For Region',
+                 xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                 yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+      }
+      
+    }
+    return(my.pie)
   })
-  
+
 }
 
-# Makes the server
 shinyServer(server)
-=======
-
-
-shinyServer(my.server)
->>>>>>> master
--- ui.R
+++ ui.R
@@ -1,22 +1,35 @@
-<<<<<<< HEAD
 library("tidyr")
+library("maps")
+library("shiny")
+library("ggplot2")
+library("dplyr")
+library("plotly")
 
-econ.csv <- read.csv("data/ECN_2012_US_52A1.csv", stringsAsFactors = FALSE)
+poverty.csv <- read.csv("data/Poverty_2015.csv", stringsAsFactors = FALSE)
 
-econ.table <- data.frame(econ.csv)
+poverty.table <- data.frame(poverty.csv)
 
-filtered.econ <- filter(econ.table, NAICS.display.label == "Finance and insurance") %>% 
-  select(GEO.display.label, PAYANN, ESTAB, EMP)
+filtered.poverty <- select(poverty.table, GEO.display.label, HC02_EST_VC01, HC02_MOE_VC01, HC03_EST_VC01, 
+                          HC03_MOE_VC01,  HC02_EST_VC03, HC02_MOE_VC03, HC03_EST_VC03, HC03_MOE_VC03) %>% 
+  filter(GEO.display.label != "Puerto Rico" & GEO.display.label != "Geography")
 
-filtered.econ$PAYANN <- as.numeric(filtered.econ$PAYANN)
-filtered.econ$ESTAB <- as.numeric(filtered.econ$ESTAB)
-filtered.econ$EMP <- as.numeric(filtered.econ$EMP)
+colnames(filtered.poverty) <- c("State", "Total Poverty", "Error.Total", "Percent Total Poverty", 
+                                "Error.Percent.Total", "Total Poverty (Under 18)", "Error.Under.18",
+                                "Percent Total Poverty (Under 18)", "Error.Percent.Under.18")
 
-filtered.econ$EMP <- cut(filtered.econ$EMP, breaks <- c(0, 10000,20000,40000,80000,160000,
-                                                        320000,640000))
+filtered.poverty$`Total Poverty` <- as.numeric(filtered.poverty$`Total Poverty`)
+filtered.poverty$Error.Total <- as.numeric(filtered.poverty$Error.Total)
+filtered.poverty$`Percent Total Poverty` <- as.numeric(filtered.poverty$`Percent Total Poverty`)
+filtered.poverty$Error.Percent.Total <- as.numeric(filtered.poverty$Error.Percent.Total)
+filtered.poverty$`Total Poverty (Under 18)` <- as.numeric(filtered.poverty$`Total Poverty (Under 18)`)
+filtered.poverty$Error.Under.18 <- as.numeric(filtered.poverty$Error.Under.18)
+filtered.poverty$`Percent Total Poverty (Under 18)` <- as.numeric(filtered.poverty$`Percent Total Poverty (Under 18)`)
+filtered.poverty$Error.Percent.Under.18 <- as.numeric(filtered.poverty$Error.Percent.Under.18)
+
 
 non <- "Not a state"
 
+# All Regions
 Northwest <-  c("Alaska", "Washington",
                 "Oregon", "Idaho", "Montana",
                 "Wyoming", non, non, non, non,
@@ -49,50 +62,110 @@ regions.long <- gather(regions, key = Region, value = State,
 
 regions.long <- filter(regions.long, State != "Not a state")
 
-colnames(filtered.econ) <- c("State", "PAYANN", "ESTAB", "EMP")
+joined.one <- left_join(regions.long, filtered.poverty, by = "State")
 
-joined.one <- left_join(regions.long, filtered.econ, by = "State")
+the.us <- map_data("state")
 
+joined.one$State <- tolower(joined.one$State)
+
+ultra.join <- left_join(joined.one, the.us, by = c("State" = "region"))
 
 # Makes a UI using a fluidPage layout
 ui <- fluidPage(
   
-  # Makes a new title Panel
-  titlePanel("2012 Economic Revenue in the United States"),
-  
-  sidebarLayout(
-    
-    # Includes three items on the left panel
-    sidebarPanel(
-      
-      # A checkboxGroupInput that lets the user select which flowers are shown
-      checkboxGroupInput("checkGroup", label = h4("Region"), 
-                         choices = list("Northwest" = "Northwest",
-                                        "South" = "South",
-                                        "Northeast" = "Northeast",
-                                        "Midwest" = "Midwest",
-                                        "Southwest" = "Southwest"),
-                         selected = c("Northwest", "South", "Northeast", "Midwest",
-                                      "Southwest"))
-      
-      # A sliderInput labeled "Sepal Length" that lets the user select the range of
-      # Sepal length to be shown
-#      sliderInput('sepal.choice', label="Sepal Length", min=round(sepal.length.range[1], 2), 
-#                  max=round(sepal.length.range[2], 2), value=round(sepal.length.range, 2)),
-      
-      # A selectInput labeled "Color Scheme", that allows the user to select which color scheme they want
-#      selectInput('color.choice', label="Color Scheme", choices=c('Primary', 'Cool', 'Hot'))
-    ),
-    
-    # The main Panel includes both the plot and table in two tabs
-    mainPanel(
-      
-      tabsetPanel(type = "tabs", 
-                  tabPanel("Plot", h4(textOutput("summary")), plotOutput("plot", click = "plot_click"), 
-                           verbatimTextOutput("info")), 
-                  tabPanel("Table", h4(textOutput("table.summary")), tableOutput("table"))
-      )
-    )
+  tabsetPanel(type = "tabs", 
+              tabPanel("Map", 
+                       sidebarLayout(
+                         
+                         # Includes three items on the left panel
+                         sidebarPanel(
+                           
+                           radioButtons("radio.region", label = h4("Region"), 
+                                        choices = list("All" = "All",
+                                                       "Northwest" = "Northwest",
+                                                       "South" = "South",
+                                                       "Northeast" = "Northeast",
+                                                       "Midwest" = "Midwest",
+                                                       "Southwest" = "Southwest"),
+                                        selected = "All"),
+                           
+                           radioButtons("radio.type", label = h4("Statistic"),
+                                        choices = list("Total Population Under Poverty Line" = 1,
+                                                       "Percentage of Population Below Poverty Line" = 2,
+                                                       "Total Population Under 18" = 3,
+                                                       "Percentage of Population Under 18 Below Poverty Line" = 4),
+                                        selected = 1),
+                           br(),
+                           p("This section shows different measurements of poverty per state,
+                              throughout the country. The darker the state, the more poverty there is - measured
+                              either by percentage or raw number. There is also the option to choose a specific
+                              region to see how a state compares to other states nearby.")
+                         ),
+                         mainPanel(
+                           h4(textOutput("summary")), plotOutput("plot")
+                         )
+                       )
+              ), 
+              tabPanel("Graphs",
+                       sidebarLayout(
+                         
+                         # Includes three items on the left panel
+                         sidebarPanel(
+                           
+                           selectInput("state1", label = h3("Select First State"), 
+                                       choices = filtered.poverty$State,
+                                       selected = "Alabama"),
+                           selectInput("state2", label = h3("Select Second State"),
+                                       choices = filtered.poverty$State,
+                                       selected = "Alaska"),
+                           radioButtons("radio", label = h4("Statistic"),
+                                        choices = list("Total Population Under Poverty Line" = 1,
+                                                       "Percentage of Population Below Poverty Line" = 2,
+                                                       "Total Population Under 18" = 3,
+                                                       "Percentage of Population Under 18 Below Poverty Line" = 4),
+                                        selected = 1),
+                           br(),
+                           p("This graph is useful for comparing how two different States compare with
+                              eachother. There are still the same four metrics from before
+                              that can be used to compare the two chosen states.")
+                         ),
+                         mainPanel(
+                           plotOutput("plot2")
+                         )
+                       )
+              ),
+              tabPanel("Regions",
+                       sidebarLayout(
+                         
+                         # Includes three items on the left panel
+                         sidebarPanel(
+                           
+                           radioButtons("statistic", label = h4("Statistic"),
+                                        choices = list("Percentage of Population Below Poverty Line" = 1,
+                                                       "Percentage of Population Under 18 Below Poverty Line" = 2),
+                                        selected = 1),
+                           
+                           radioButtons("radio.for.regions", label = h4("Region"), 
+                                        choices = list("All" = "All",
+                                                       "Midwest" = "Midwest",
+                                                       "Northeast" = "Northeast",
+                                                       "Northwest" = "Northwest",
+                                                       "South" = "South",
+                                                       "Southwest" = "Southwest"),
+                                        selected = "All"),
+                           br(),
+                           p("This section compares the regions altogether, and breaks down a region
+                              into the states that contribute to its overall poverty. If a specific region
+                              is chosen, a pie graph will show the percentage of a the chosen statistic for
+                              each state in relation to the region as a whole.")
+                         ),
+                         mainPanel(
+                           plotOutput("plot3"),
+                           plotlyOutput("pie.plot")
+                         )
+                       )   
+                       
+              )
   )
 )
 
