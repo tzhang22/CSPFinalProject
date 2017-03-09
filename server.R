ce140ba81888b18ea001cf853c9808cf40d1caf9
Zhihao Yang <zhihay@uw.edu>
2017-03-08 21:45:27 -0800

    sam

--- server.R
+++ server.R
@@ -1,11 +1,87 @@
 # Load the shiny, ggplot2, and dplyr libraries
+library("tidyr")
+library("maps")
 library("shiny")
 library("ggplot2")
 library("dplyr")
+library("plotly")
 
-tax.data <- read.csv("data/Taxes_2015.csv", stringsAsFactors = FALSE)
 income.data <- read.csv("data/Income_Employment_2015.csv", stringsAsFactors = FALSE)
-poverty.data <- read.csv("data/Poverty_2015.csv", stringsAsFactors = FALSE)
+
+# It filter the information about state where user choose.
+my.server <- function(input, output) {
+  filtered <- reactive({
+    new <- filter(long.join, input$state == State)
+    print(input$state)
+    
+    return(new)
+  })
+  
+# This paragraph will just make sure that the region user choose will get some outputs.
+  filtered.map <- reactive({
+    new.table <- State.join
+    if (input$radio.region != "All") {
+      new.table <- filter(State.join, Region %in% input$radio.region)
+    }
+    return(new.table)
+  })
+  
+  
+  output$description1 <- renderText({
+    
+    message <- paste0("This graph shows the level of ", input$labor.type," in 2015 in ",input$radio.region, " part. From lightest color to darkest color,
+                      it shows the state which has least percentage of ",input$labor.type, " to state which has largest percentage of ", input$labor.type)
+    
+    return(message)
+  })
+  
+  output$description2 <- renderText({
+    
+    message <- paste0("This bar graph shows the percentage of three categories(Employed labor force,
+                      Unemployed labor force, Armed labor force) in ",input$state)
+    
+    return(message)
+  })
+  
+  # This graph shows the level of labor force in 2015 in one region where user choose. From lightest color to darkest color,
+  # it shows the state which has least percentage of labor force type to state which has largest percentage of labor force type.
+  output$plot1 <- renderPlot({
+    
+    if (input$labor.type == "Employed labor force") {
+      p <- ggplot(data = filtered.map()) +
+        geom_polygon(aes(x = long, y = lat, group = group, color = Region, 
+                         fill = `Employed labor force`), color = "black") +
+        scale_fill_continuous(low = "#2A9B13", high = "#901010")
+    } else if (input$labor.type == "Unemployed labor force") {
+      p <- ggplot(data = filtered.map()) +
+        geom_polygon(aes(x = long, y = lat, group = group, color = Region, 
+                         fill = `Unemployed labor force`), color = "black") +
+        scale_fill_continuous(low = "#2A9B13", high = "#672A70")
+    } else {
+      p <- ggplot(data = filtered.map()) +
+        geom_polygon(aes(x = long, y = lat, group = group, 
+                         fill = `Armed labor force`), color = "black") +
+        scale_fill_continuous(low = "#2A9B13", high = "#138A6E")
+    }
+    
+    p <- p + coord_fixed(ratio = 1.0)
+    
+    return(p)
+  })
+  
+  # This bar graph shows the percentage of three categories(Employed labor force,
+  # Unemployed labor force, Armed labor force) in state that user choose.
+  output$plot2 <- renderPlot({
+    my.plot <- ggplot(data = filtered()) + 
+      geom_bar(mapping = aes(x = type.of.force, y = value, fill = type.of.force),
+               stat = "identity")
+    
+    return(my.plot)
+    
+  })
+  
+  
+}
 
 
 shinyServer(my.server)
--- ui.R
+++ ui.R
@@ -1,11 +1,123 @@
 # Load the shiny, ggplot2, and dplyr libraries
+library("tidyr")
+library("maps")
 library("shiny")
 library("ggplot2")
 library("dplyr")
+library("plotly")
 
-tax.data <- read.csv("data/Taxes_2015.csv", stringsAsFactors = FALSE)
 income.data <- read.csv("data/Income_Employment_2015.csv", stringsAsFactors = FALSE)
-poverty.data <- read.csv("data/Poverty_2015.csv", stringsAsFactors = FALSE)
+
+
+
+filtered.employment <- select(income.data, GEO.display.label,HC03_VC06, HC03_VC07, HC03_VC08) %>% 
+  filter(GEO.display.label != "Puerto Rico" & GEO.display.label != "Geography")
+
+colnames(filtered.employment) <- c("State", "Employed labor force", "Unemployed labor force", "Armed labor force")
+
+
+filtered.employment$`Employed labor force` <- as.numeric(filtered.employment$`Employed labor force`)
+filtered.employment$`Unemployed labor force` <- as.numeric(filtered.employment$`Unemployed labor force`)
+filtered.employment$`Armed labor force` <- as.numeric(filtered.employment$`Armed labor force`)
+
+
+non <- "Not a state"
+
+
+Northwest <-  c("Alaska", "Washington",
+                "Oregon", "Idaho", "Montana",
+                "Wyoming", non, non, non, non,
+                non, non, non)
+South <- c("Florida", "Mississippi",
+           "Louisiana", "Alabama", "Georgia",
+           "South Carolina", "North Carolina",
+           "Tennessee", "Arkansas", "Kentucky",
+           "Virginia", non, non)
+Northeast <- c("Maine", "New Hampshire",
+               "Vermont", "Massachusetts",
+               "Rhode Island", "Connecticut",
+               "New York", "Pennsylvania", "Delaware",
+               "District of Columbia", "Maryland",
+               "New Jersey", "West Virginia")
+Midwest <- c("Ohio", "Indiana", "Michigan",
+             "Illinois", "Wisconsin",
+             "Missouri", "Iowa", "Minnesota",
+             "Kansas", "Nebraska", "South Dakota",
+             "North Dakota", non)
+Southwest <- c("California", "Hawaii", "Nevada",
+               "Utah", "Colorado",
+               "Arizona", "New Mexico",
+               "Oklahoma", "Texas", non, non, non, non)
+
+regions <- data.frame(Northwest, South, Northeast, Midwest, Southwest)
+# Gather different region into one.
+regions.long <- gather(regions, key = Region, value = State,
+                       Northwest, South, Northeast, Midwest, Southwest)
+
+regions.long <- filter(regions.long, State != "Not a state")
+
+# join two data frame into one by state name.
+joined <- left_join(regions.long, filtered.employment, by = "State")
+
+# gather three categories into one columns with value besides it.
+long.join <- gather(joined, "type.of.force", "value", `Employed labor force`,
+                    `Unemployed labor force`, `Armed labor force`)
+
+long.join$State <- tolower(long.join$State)
+
+us.maps <- map_data("state")
+
+joined$State <- tolower(joined$State)
+
+# Join us.maps which has logitude and latitude and long.join into one data frame.
+State.join <- left_join(joined, us.maps, by = c("State" = "region"))
+
+
+
+
+
+my.ui <- fluidPage(
+  
+  # Makes a new title Panel
+  titlePanel("2015 Employment status in the United States"),
+  
+  tabsetPanel(type = "tabs", 
+              tabPanel("Map", 
+                       sidebarLayout(
+                         
+                         # Includes three items on the left panel
+                         sidebarPanel(
+                           # User can choose region.
+                           radioButtons("radio.region", label = h4("Region"), 
+                                        choices = list("All" = "All",
+                                                       "Northwest" = "Northwest",
+                                                       "South" = "South",
+                                                       "Northeast" = "Northeast",
+                                                       "Midwest" = "Midwest",
+                                                       "Southwest" = "Southwest"),
+                                        selected = "All"),
+                           
+                           # User can choose labor force type.
+                           selectInput("labor.type", label = h3("Labor.force.type"), 
+                                       choices = c("Employed labor force", "Unemployed labor force", "Armed labor force"),
+                                       selected = "Employed labor force"),
+                           
+                           # User can select a state.
+                           selectInput("state", label = h3("Select State"), 
+                                     choices = joined$State,
+                                     selected = "Alabama")
+                         ),
+                         mainPanel(
+                           textOutput("description1"),
+                           plotOutput("plot1"),
+                           textOutput("description2"),
+                           plotOutput("plot2")
+                         )
+                       )
+              )
+  )
+)
+
 
 
 shinyUI(my.ui)
